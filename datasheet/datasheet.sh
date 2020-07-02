9fc03107c3149cca3f9116431290428035c31468
daniw <dani.winz@bluewin.ch>
2020-07-02 05:12:11 +0200

    Datasheets started

---
+++ datasheet/datasheet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# MSP430x2xx Family User's Guide
+wget https://www.ti.com/lit/ug/slau144j/slau144j.pdf
+# MSP430G2553 Datasheet
+wget https://www.ti.com/lit/ds/symlink/msp430g2553.pdf
+# MSP-EXP430G2 LaunchPad
+wget https://www.ti.com/lit/ug/slau318g/slau318g.pdf
