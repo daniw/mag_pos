1b065cae2bc60f60634a456793079ab754bcf211
daniw <dani.winz@bluewin.ch>
2020-07-10 21:24:55 +0200

    All calibrated DCO frequencies added to header file

--- sw/common/clock.c
+++ sw/common/clock.c
@@ -9,8 +9,8 @@
 void init_clock(void){
     /* Load calibration data for 16MHz DCO frequency */
     DCOCTL  = 0x00; // Select lowest DCOx and MODx settings
-    BCSCTL1 = CALBC1_16MHZ; // Set range
-    DCOCTL  = CALDCO_16MHZ; // Set DCO step + modulation
+    BCSCTL1 = CALBC1_INIT; // Set range
+    DCOCTL  = CALDCO_INIT; // Set DCO step + modulation
     /* Use VCO as main clock without Prescaler */
     BCSCTL2 = SELM_0 | DIVM_0;
     return;
--- sw/common/clock.h
+++ sw/common/clock.h
@@ -10,7 +10,25 @@
 
 #include <msp430.h>
 
-#define CLOCK_FREQ 16000000
+#ifndef CLOCK_FREQ
+    #define CLOCK_FREQ 16000000
+#endif
+
+#if CLOCK_FREQ == 1000000
+    #define CALBC1_INIT CALBC1_1MHZ
+    #define CALDCO_INIT CALDCO_1MHZ
+#elif CLOCK_FREQ == 8000000
+    #define CALBC1_INIT CALBC1_8MHZ
+    #define CALDCO_INIT CALDCO_8MHZ
+#elif CLOCK_FREQ == 12000000
+    #define CALBC1_INIT CALBC1_12MHZ
+    #define CALDCO_INIT CALDCO_12MHZ
+#elif CLOCK_FREQ == 16000000
+    #define CALBC1_INIT CALBC1_16MHZ
+    #define CALDCO_INIT CALDCO_16MHZ
+#else
+    #error "Invalid DCO frequency selected. Check CLOCK_FREQ value. "
+#endif
 
 void init_clock(void);
 
