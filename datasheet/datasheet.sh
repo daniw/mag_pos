3da55a16164fd14b2f6681117038cba71ea71695
daniw <dani.winz@bluewin.ch>
2020-08-31 15:49:17 +0200

    MSP430FR2355 datasheet, family's user guide and launchpad documentation

--- datasheet/datasheet.sh
+++ datasheet/datasheet.sh
@@ -8,6 +8,12 @@ wget -nc -O uC/family_guide_slau144j.pdf https://www.ti.com/lit/ug/slau144j/slau
 wget -nc -P uC https://www.ti.com/lit/ds/symlink/msp430g2553.pdf
 # MSP-EXP430G2 LaunchPad
 wget -nc -O uC/msp-exp430g2_launchpad_slau318g.pdf https://www.ti.com/lit/ug/slau318g/slau318g.pdf
+# MSP430FR4xx and MSP430FR2xx Family User's Guide
+wget -nc -O uC/family_guide_FR_slau445i.pdf https://www.ti.com/lit/ug/slau445i/slau445i.pdf
+# MSP430FR2355 Datasheet
+wget -nc -P uC https://www.ti.com/lit/ds/symlink/msp430fr2355.pdf
+# MSP430FR2355 Launchpad
+wget -nc -O uC/msp-exp430fr2355_launchpad_slau680.pdf https://www.ti.com/lit/ug/slau680/slau680.pdf
 
 ## Sensor
 mkdir -p sensor
