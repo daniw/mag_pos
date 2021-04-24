d9ae560986821110bad847f414a9f680cd534eae
daniw <dani.winz@bluewin.ch>
2021-04-24 21:51:26 +0200

    Analysis script

---
+++ meas/mag_pos_analyze.m
@@ -0,0 +1,46 @@
+% Clean workspace
+clc;
+clear;
+%close all;
+
+% Parameters
+WRAP_THRESHOLD = 2^15;    % Threshold for wraparound correction
+WRAP_INTMAX    = 2^16;
+
+% Read data
+file = 'test_001';
+data = dlmread([file '.csv'], ',', [1, 0, inf, 5]);
+% Wraparound correction
+data(data>WRAP_THRESHOLD) = data(data>WRAP_THRESHOLD)-WRAP_INTMAX;
+
+titles = {'Temperature', 'Flux X', 'Flux Y', 'Flux Z', 'Linear', 'Angular'};
+
+% Raw plot
+for i=1:6
+    figure(i);
+    plot(data(:,i), 'LineWidth', 2);
+    grid on; grid minor;
+    title(['Raw plot ' cell2mat(titles(i))]);
+end;
+
+
+% Value extraction
+temp = data(:,1);
+flux_x = data(:,2);
+flux_y = data(:,3);
+flux_z = data(:,4);
+linear = data(:,5);
+angular = data(:,6);
+
+plots = [2, 3, 5, 6];
+
+% Plot
+figure(100);
+for i=plots
+    subplot(length(plots), 1, find(plots==i));
+    plot(data(:,i), 'LineWidth', 2);
+    grid on; grid minor;
+    title(cell2mat(titles(i)));
+end;
+
+print('-dpng', file);
